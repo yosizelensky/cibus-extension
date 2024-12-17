d9872afa48a227b397ca29dee5da6532b7edb0f8
Roey Darwish Dror <roey.ghost@gmail.com>
2024-12-17 22:50:24 +0200

    Version bump

--- user.js
+++ user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://consumers.pluxee.co.il/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 20/09/2024, 15:19:45
 // ==/UserScript==
